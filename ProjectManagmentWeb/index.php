75318b4332d0516aeaad56cbc8f63b129abd5071
Josip Bošnjak <jbosnjak@unipu.hr>
2020-04-20 19:32:40 +0200

    added index login and register php

---
+++ ProjectManagmentWeb/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project Managment App</title>
+</head>
+<body>
+<div>
+
+<a href="login.php">Login</a>
+
+<a href="register.php">Sign in</a>
+
+
+
+</div>
+
+</body>
+</html>
