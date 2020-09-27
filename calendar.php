e5106b54cc6aacd07cca26b3f6506c8bc5bd6628
Josip Bošnjak <jbosnjak@unipu.hr>
2020-09-27 11:54:58 +0200

    added pagination links, links with years, and section for square content

--- calendar.php
+++ calendar.php
@@ -10,6 +10,10 @@
     ?>
 </head>
 <body>
+<a href="#nextyear">Next year</a>
+<a href="#previousyear">Previous year</a>
+<a href="#firstyear">First year</a>
+
 <section id="year">
 <style>
  #content{
@@ -25,7 +29,24 @@
 
 
 ?>
+<?php 
+
+for ($i=1960; $i < 2030; $i++) { 
+    echo  "<a id='content' href='". $i."'>".$i."</a>";
+}
+
+?>
 </section>
+<style>
+#square{
+    margin-left:50%;
+    margin-top:-180%;
     
+}
+</style>
+<section id="square">
+<p>Some text</p>
+
+</section>
 </body>
 </html>
